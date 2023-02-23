2f047954ac576fef1778b8cee4cb152753c57263
Abhishek V <abhishek.v@smartfoodsafe.com>
2023-02-24 01:41:23 +0530

    finished

--- index.js
+++ index.js
@@ -1,20 +1,64 @@
-#!/usr/bin/node
+#!/usr/local/bin/node
 import { chromium } from "playwright";
 import inquirer from "inquirer";
 import clipboardy from "clipboardy";
+import { readFileSync, writeFileSync } from "fs";
+import lodash from "lodash";
+import cliProgress from "cli-progress";
+import { promisify } from "util";
+import { exec } from "child_process";
 
+const execAsync = promisify(exec);
+
+const bar1 = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
+
+function getLocale(language) {
+  switch (language) {
+    case "FRENCH":
+      return "fr";
+    case "SPANISH":
+      return "es";
+    default:
+      return "en";
+  }
+}
+
+function flatten(object, addToList, prefix) {
+  Object.keys(object).map((key) => {
+    if (object[key] === null) {
+      addToList[prefix + key] = "";
+    } else if (object[key] instanceof Array) {
+      for (i in object[key]) {
+        flatten(object[key][i], addToList, prefix + key + "." + i);
+      }
+    } else if (
+      typeof object[key] == "object" &&
+      !object[key].toLocaleDateString
+    ) {
+      flatten(object[key], addToList, prefix + key + ".");
+    } else {
+      addToList[prefix + key] = object[key];
+    }
+  });
+  return addToList;
+}
 (async () => {
+  let moduleEnJson;
+  const { stdout } = await execAsync("npx playwright install");
+
   const a = await inquirer.prompt([
     {
       type: "input",
       name: "sourcePath",
       message: "Enter source JSON file path (English)",
-      default: "../../../locales/en/appModule.json",
+      default: "./appModule.json",
       validate: (value) => {
         try {
-          require(value);
+          const a = readFileSync(value);
+          moduleEnJson = JSON.parse(a);
           return true;
         } catch (error) {
+          console.log(error);
           return "Please enter valid path";
         }
       },
@@ -26,56 +70,60 @@ import clipboardy from "clipboardy";
       choices: ["FRENCH", "SPANISH"],
     },
   ]);
-  console.log(a);
-  const moduleEnJson = require(a.sourcePath);
 
   const browser = await chromium.launch({
-    headless: false,
+    headless: true,
   });
   const page = await browser.newPage();
-  const lodash = require("lodash");
-  const { flatten } = require("./utills");
 
   const engJson = flatten(moduleEnJson, [], "");
   let frJson = {};
 
-  await page.goto("https://translate.google.com/");
+  bar1.start(Object.keys(engJson).length, 0);
+  let completed = 0;
+
+  await page.goto(
+    `https://translate.google.com/?sl=auto&tl=${getLocale(
+      a.language
+    )}&op=translate`
+  );
 
   await page.waitForSelector("text=English >> visible=true");
-
-  await page.getByRole("button", { name: "More target languages" }).click();
-
-  await page
-    .getByRole("main", { name: "Text translation" })
-    .getByText(a.language)
-    .nth(2)
-    .click();
-  await page
-    .getByRole("main", { name: "Text translation" })
-    .getByText(a.language)
-    .nth(2)
-    .click();
 
   await page.waitForTimeout(2000);
 
   let clear = false;
-  for (const key of Object.keys(engJson)) {
+  const paginated = [];
+
+  Object.keys(engJson).forEach((key, index) => {
+    if (index % 50 === 0) {
+      paginated.push([]);
+    }
+    paginated[paginated.length - 1].push(key);
+  });
+
+  for (const pageKeys of paginated) {
     if (clear) {
       await page.getByRole("button", { name: "Clear source text" }).click();
     }
     await page
       .getByRole("combobox", { name: "Source text" })
-      .fill(engJson[key]);
+      .fill(pageKeys.map((key) => lodash.get(engJson, key)).join("\n"));
 
     await page
       .getByRole("button", { name: "Copy translation" })
       .first()
       .click();
 
+    completed += pageKeys.length;
+
+    bar1.update(completed);
     const string = await clipboardy.read();
 
-    console.log(string);
-    frJson[key] = string;
+    string.split("\n").forEach((string, index) => {
+      const key = pageKeys[index];
+      lodash.set(frJson, key, string);
+    });
 
     clear = true;
   }
@@ -87,8 +135,27 @@ import clipboardy from "clipboardy";
       lodash.set(json, key, element);
     }
   }
-  await clipboardy.write(JSON.stringify(json, null, 2));
-  console.log(json);
 
   await browser.close();
+
+  bar1.stop();
+
+  const b = await inquirer.prompt([
+    {
+      type: "input",
+      name: "destinationPath",
+      message: "Enter destination JSON file path",
+      default: "./appModule.json",
+      validate: (value) => {
+        try {
+          return true;
+        } catch (error) {
+          console.log(error);
+          return "Please enter valid path";
+        }
+      },
+    },
+  ]);
+  writeFileSync(b.destinationPath, JSON.stringify(json, null, 2));
+  console.log(`\x1b[32m%s\x1b[0m`, `File written to ${b.destinationPath}`);
 })();
